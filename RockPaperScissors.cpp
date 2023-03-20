98cbb6194673fce2cddb5327b10cb8bb0eb2e009
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-03-20 12:16:42 +0200

    getComputer choice function

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 char getUserChoice();
 char getComputerChoice();
@@ -15,6 +16,9 @@ int main()
     showChoice(player);
 
     computer = getComputerChoice();
+    std::cout << "Computer's choice: ";
+    showChoice(computer);
+
 }
 
 char getUserChoice(){
@@ -36,6 +40,14 @@ char getUserChoice(){
 
 char getComputerChoice(){
 
+    srand(time(0));
+    int num = rand() % 3 + 1;
+
+    switch(num) {
+        case 1: return 'r';
+        case 2: return 'p';
+        case 3: return 's';
+    }
 }
 
 void showChoice(char choice){
